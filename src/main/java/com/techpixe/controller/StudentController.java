7dbcb2766a0efda82f7f561573db57a6bd446556
prasad <sldprasad022@gmail.com>
2024-04-17 22:49:46 +0530

    I have added delete method in controller

--- src/main/java/com/techpixe/controller/StudentController.java
+++ src/main/java/com/techpixe/controller/StudentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +45,12 @@ public class StudentController
 	{
 		List<Student> fetchAll = studentService.all();
 		return new ResponseEntity<List<Student>>(fetchAll,HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/deleteById/{id}")
+	public ResponseEntity<Void> deleteById(Long id)
+	{
+		//studentService.ge
 	}
 	
 	
